fe09303bfaa75cf9da464ece596d4fa5e71d8c5f
Arjit kiledar <kiledar.a@northeastern.edu>
2018-11-28 11:28:53 -0500

    Adding router to different component and header component

--- Stream/client/src/components/App.js
+++ Stream/client/src/components/App.js
@@ -1,24 +1,29 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import StreamDelete from "./streams/StreamDelete";
+import StreamCreate  from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import Header from "./Header";
 
-const PageOne = () => {
-    return <div> PageOne </div>
-}
 
-const PageTwo = () => {
-    return <div> PageTwo </div>
-}
 
 const App = () =>{
 
     return (
-    <div>
-      <BrowserRouter>
-      <div>
-        <Route path = "/" exact component = {PageOne} />
-        <Route path ="/pagetwo" component = {PageTwo} />
+    <div className = "ui container">
+       
+        <BrowserRouter>
+        <div>
+            <Header />
+            <Route path="/" exact component = {StreamList} />
+            <Route path="/streams/show" exact component = {StreamShow} />
+            <Route path="/streams/delete" exact component = {StreamDelete} />
+            <Route path="/streams/edit" exact component = {StreamEdit} />
+            <Route path="/streams/new" exact component = {StreamCreate} />
         </div>
-      </BrowserRouter>  
+        </BrowserRouter>
     </div>);
 }
 
